dd0dab1163ecaf74f2a422defdbf781a7eb2d40e
Melanie Archer <job@twobanjos.com>
2011-08-08 14:51:36 -0700

    Remove unused script; iterate through color names for backgrounds.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
-function drawCootie() {
-var canvas, ctx, width, height, grad,strokeColor,rand, fillColors;
-width=140;
-height=140;
-strokeColor='#ccc';
-fillColors=['red', 'green', 'blue', 'yellow'];
-for (i=0; i<4; i++) {
-canvas=$(document.createElement('canvas'))
-.prop({'width':width, 'height':height})
-.addClass('closed')
-.appendTo('.cootie');
-}
-if($('.cootie canvas', '#main').length) {
- $('.cootie canvas', '#main').each(function(){
-   rand = Math.ceil(Math.random()*fillColors.length);
-  console.log(fillColors[rand]);
-   ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
-   ctx.strokeStyle=strokeColor;
-        grad = ctx.createLinearGradient(0, 0, width, height);
-grad.addColorStop(0, '#fff');
-grad.addColorStop(1, fillColors[rand]); //todo: cycle through bg colors through selectors
-ctx.fillStyle = grad;
-ctx.fillRect(0, 0, width, height);
- 
-      
-        //canvas.live('click', function() {
-         //   playCootie(stage_value);
-       // });
-    });
-}}
-$(function() {
- //   var canvas, ctx, width, height, padding, colors, stage_value;
-drawCootie();
-   // 
- 
-});
 
+
+function drawCootie() {
+    var canvas, ctx, width, height, grad, strokeColor, fillColors, rand, squareColor;
+    width = 140;
+    height = 140;
+    strokeColor = '#ccc';
+    fillColors = ['red', 'green', 'blue', 'yellow'];
+    for (i = 0; i < 4; i++) {
+        canvas = $(document.createElement('canvas')).prop({
+            'width': width,
+            'height': height
+        }).addClass('closed').appendTo('.cootie');
+    }
+    if ($('.cootie canvas', '#main').length) {
+        $('.cootie canvas', '#main').each(function() {
+            rand = Math.floor(Math.random() * fillColors.length);
+            squareColor = fillColors.splice(rand, 1)[0];
+            ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
+            ctx.strokeStyle = strokeColor;
+            grad = ctx.createLinearGradient(0, 0, width, height);
+            grad.addColorStop(0, '#fff');
+            grad.addColorStop(1, squareColor);
+            ctx.fillStyle = grad;
+            ctx.fillRect(0, 0, width, height);
+
+
+            //canvas.live('click', function() {
+            //   playCootie(stage_value);
+            // });
+        });
+    }
+}
+$(function() {
+    //   var canvas, ctx, width, height, padding, colors, stage_value;
+    drawCootie();
+    // 
+});
