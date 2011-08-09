44fb0c1c1356ace05d4c8a7c1f8b475e0eb946b9
Melanie Archer <job@twobanjos.com>
2011-08-08 18:06:13 -0700

    Disabled gradient-trying to discover bg color on click; temp. teal bg (swear I'll remove it)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
 
+function playCootie(e,ctx,clr){
+var x, y, hex;
+x=e.pageX;
+y=e.pageY;
+c = ctx.getImageData(x, y, 1, 1).data;
+hex = '#' + clr;
+console.log('!!'+x+','+y+','+hex);
+}
 
 function drawCootie() {
     var canvas, ctx, width, height, grad, strokeColor, fillColors, rand, squareColor;
@@ -19,17 +32,17 @@ function drawCootie() {
             squareColor = fillColors.splice(rand, 1)[0];
             ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
             ctx.strokeStyle = strokeColor;
-            grad = ctx.createLinearGradient(0, 0, width, height);
-            grad.addColorStop(0, '#fff');
-            grad.addColorStop(1, squareColor);
-            ctx.fillStyle = grad;
+           // grad = ctx.createLinearGradient(0, 0, width, height);
+           // grad.addColorStop(0, '#fff');
+           // grad.addColorStop(1, squareColor);
+            ctx.fillStyle = squareColor;
             ctx.fillRect(0, 0, width, height);
-
-
-            //canvas.live('click', function() {
-            //   playCootie(stage_value);
-            // });
         });
+        $(this).click( function(e) {
+            //   playCootie(stage_value);
+            playCootie(e, ctx,squareColor);
+          });
+            //how to attach event to each square?
     }
 }
 $(function() {
