db39ea9dae8b998d6cb3b9ce65cc036cad26049b
Melanie Archer <job@twobanjos.com>
2011-08-10 17:36:23 -0700

    Got the color names, and some detection of what was clicked. Next: animate per :nth-child.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,62 @@
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
-function rgbToHex(r, g, b) {
-    if (r > 255 || g > 255 || b > 255)
-        throw "Invalid color component";
-    return ((r << 16) | (g << 8) | b).toString(16);
+
+function playCootie(whichColor) {
+    //get letters in color name
+    for (var i = 0, len = whichColor.length; i < len; i++) {
+        var li = '<li>' + whichColor[i] + '</li>';
+        $('#progress').append(li);
+        moveCootie(i + 1);
+    }
 }
 
-function playCootie(e,ctx,clr){
-var x, y, hex;
-x=e.pageX;
-y=e.pageY;
-c = ctx.getImageData(x, y, 1, 1).data;
-hex = '#' + clr;
-console.log('!!'+x+','+y+','+hex);
+function moveCootie(howMuch) {
+    var i, x, y;
+    for (i = 1; i <= 4; i++) {
+        switch (i) {
+        case 1:
+            x = '';
+            y = '-=20';
+            break;
+        case 2:
+            x = '+=20';
+            y = '';
+            break;
+        case 3:
+            x = '';
+            y = '-=20';
+            break;
+        case 4:
+            x = '';
+            y = '-=20';
+            break;
+        }
+        //dynamically assign values to top, left per nth-child selector.
+        //.cootie canvas:nth-child(1)--move up; top -=20px
+        //.cootie canvas:nth-child(2)--move right; left +=20px
+        //.cootie canvas:nth-child(3)--move down; top +=20px
+        //.cootie canvas:nth-child(4)--move left; left -=20px
+   // }
+   console.log('x'+x+'y'+ y);
+}
+
+
+//$('.cootie canvas', '#main')
+//.removeClass('closed')
+//.addClass('opened')
+//.animate({
+//top: y,
+//left: x},500, 'linear',function() {}
+//); 
 }
 
 function drawCootie() {
-    var canvas, ctx, width, height, grad, strokeColor, fillColors, rand, squareColor;
+    var $canvas, ctx, width, height, grad, strokeColor, fillColors, rand, squareColor;
     width = 140;
     height = 140;
-    strokeColor = '#ccc';
+    strokeColor = '#999';
     fillColors = ['red', 'green', 'blue', 'yellow'];
     for (i = 0; i < 4; i++) {
-        canvas = $(document.createElement('canvas')).prop({
+        $canvas = $(document.createElement('canvas')).prop({
             'width': width,
             'height': height
         }).addClass('closed').appendTo('.cootie');
@@ -32,21 +67,17 @@ function drawCootie() {
             squareColor = fillColors.splice(rand, 1)[0];
             ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
             ctx.strokeStyle = strokeColor;
-           // grad = ctx.createLinearGradient(0, 0, width, height);
-           // grad.addColorStop(0, '#fff');
-           // grad.addColorStop(1, squareColor);
             ctx.fillStyle = squareColor;
             ctx.fillRect(0, 0, width, height);
+            $(this).addClass(squareColor);
         });
-        $(this).click( function(e) {
-            //   playCootie(stage_value);
-            playCootie(e, ctx,squareColor);
-          });
-            //how to attach event to each square?
+        $(this).click(function(e) {
+            var clr = e.target.className.split(' ').pop();
+            //get last class name, since that has the color
+            playCootie(clr);
+        });
     }
 }
 $(function() {
-    //   var canvas, ctx, width, height, padding, colors, stage_value;
     drawCootie();
-    // 
 });
