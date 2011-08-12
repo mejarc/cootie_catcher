9627cce893f7effd5e2196d46d89ebaa9474db6a
Melanie Archer <job@twobanjos.com>
2011-08-11 17:25:35 -0700

    Still trying to animate the open/close canvases per # of letters

--- js/script.js
+++ js/script.js
@@ -1,43 +1,18 @@
+
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
 
-function playCootie(whichColor) {
-    //get letters in color name
-    for (var i = 0, len = whichColor.length; i < len; i++) {
-        var li = '<li>' + whichColor[i] + '</li>';
-        $('#progress').append(li);
-        moveCootie(i + 1);
-    }
-}
-
 function moveCootie(howMuch) {
-    var i, x, y;
-    for (i = 1; i <= 4; i++) {
-        switch (i) {
-        case 1:
-            x = '';
-            y = '-=20';
-            break;
-        case 2:
-            x = '+=20';
-            y = '';
-            break;
-        case 3:
-            x = '';
-            y = '-=20';
-            break;
-        case 4:
-            x = '';
-            y = '-=20';
-            break;
-        }
-        //dynamically assign values to top, left per nth-child selector.
-        //.cootie canvas:nth-child(1)--move up; top -=20px
-        //.cootie canvas:nth-child(2)--move right; left +=20px
-        //.cootie canvas:nth-child(3)--move down; top +=20px
-        //.cootie canvas:nth-child(4)--move left; left -=20px
-   // }
-   console.log('x'+x+'y'+ y);
-}
+var $j, h, w,m;
+$j='.cootie canvas';
+h=280;
+w=280;
+m=20;
+$j.css(
+{ 'width': w,
+            'height': h,
+            'margin-right':m+'px'}
+);
+
 
 
 //$('.cootie canvas', '#main')
@@ -48,17 +23,27 @@ function moveCootie(howMuch) {
 //left: x},500, 'linear',function() {}
 //); 
 }
+function playCootie(whichColor) {
+    //get letters in color name
+    for (var i = 0, len = whichColor.length; i < len; i++) {
+        var li = '<li>' + whichColor[i] + '</li>';
+        $('#progress').append(li);
+        moveCootie(i + 1);
+    }
+}
+
+
 
 function drawCootie() {
-    var $canvas, ctx, width, height, grad, strokeColor, fillColors, rand, squareColor;
-    width = 140;
-    height = 140;
+    var $canvas, ctx, w, h, grad, strokeColor, fillColors, rand, squareColor;
+    w = 140;
+    h = 140;
     strokeColor = '#999';
     fillColors = ['red', 'green', 'blue', 'yellow'];
-    for (i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) {
         $canvas = $(document.createElement('canvas')).prop({
-            'width': width,
-            'height': height
+            'width': w,
+            'height': h
         }).addClass('closed').appendTo('.cootie');
     }
     if ($('.cootie canvas', '#main').length) {
@@ -68,7 +53,7 @@ function drawCootie() {
             ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
             ctx.strokeStyle = strokeColor;
             ctx.fillStyle = squareColor;
-            ctx.fillRect(0, 0, width, height);
+            ctx.fillRect(0, 0, w, h);
             $(this).addClass(squareColor);
         });
         $(this).click(function(e) {
