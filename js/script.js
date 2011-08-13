43919be23ccb5a8eeb81cf284f0c16f09d97dc5a
Melanie Archer <job@twobanjos.com>
2011-08-12 17:10:44 -0700

    Give up trying deep clone of Canvas objects.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
-
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
 
 function moveCootie(howMuch) {
-var $j, h, w,m;
-$j='.cootie canvas';
-h=280;
-w=280;
-m=20;
-$j.css(
-{ 'width': w,
-            'height': h,
-            'margin-right':m+'px'}
-);
+    var $j, h, w, m;
+    $j = '.cootie canvas';
+    h = 280;
+    w = 280;
+    m = 20;
+    $j.css({
+        'width': w,
+        'height': h,
+        'margin-right': m + 'px'
+    });
 
-
-
-//$('.cootie canvas', '#main')
-//.removeClass('closed')
-//.addClass('opened')
-//.animate({
-//top: y,
-//left: x},500, 'linear',function() {}
-//); 
 }
+
 function playCootie(whichColor) {
-var i=0;
+    var i = 0;
     //get letters in color name
     for (; i < whichColor.length; i++) {
         var li = '<li>' + whichColor[i] + '</li>';
@@ -33,11 +24,9 @@ var i=0;
     }
 }
 
-
-
 function drawCootie() {
     var i, $canvas, ctx, w, h, grad, strokeColor, fillColors, rand, squareColor;
-    i=0;
+    i = 0;
     w = 140;
     h = 140;
     strokeColor = '#999';
@@ -46,9 +35,10 @@ function drawCootie() {
         $canvas = $(document.createElement('canvas')).prop({
             'width': w,
             'height': h
-        }).addClass('closed').appendTo('.cootie');
+        }).appendTo('.cootie');
     }
     if ($('.cootie canvas', '#main').length) {
+    console.log($canvas);
         $('.cootie canvas', '#main').each(function() {
             rand = Math.floor(Math.random() * fillColors.length);
             squareColor = fillColors.splice(rand, 1)[0];
@@ -64,6 +54,8 @@ function drawCootie() {
             playCootie(clr);
         });
     }
+
+
 }
 $(function() {
     drawCootie();
