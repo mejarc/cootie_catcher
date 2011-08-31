c2199953f0e576724fefc735775d8ae50457b72e
Melanie Archer <job@twobanjos.com>
2011-08-30 17:37:43 -0700

    The jQuery UI switchClass method is pretty neat. Doesn't fix everything.

--- js/script.js
+++ js/script.js
@@ -1,71 +1,63 @@
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
 
 function moveCootie(times) {
-    var $cootie, degree, i, j, interval;
-    $cootie = $('.cootie.opened');
-    i = 1;
-    j = 0;
-    degree = 15;
+    var len = times,
+        i = 0,
+        $cootie = $('.cootie.opened');
+    for (; i < len; i++) { //Blog: why won't this loop???
 
-    function skew() {
-        $cootie.css('-webkit-transform', 'skew(-' + degree + 'deg)').css('-moz-transform', 'skew(-' + degree + 'deg)').css('transform', 'skew(-' + degree + 'deg)'); //skew one way
-        $cootie.css('-webkit-transform', 'skew(' + degree + 'deg)').css('-moz-transform', 'skew(' + degree + 'deg)').css('transform', 'skew(' + degree + 'deg)'); //skew the other
-        //todo:opera
+            $('.cootie.opened').switchClass('skew', 'revskew', 500);
+            console.log($cootie.attr('class'));
+
     }
-    interval = setInterval(function(){
-    skew();
-    }, 1000);
 
-    if (i < times) {//Blog: why won't this loop???
-        skew();
-    }++i;
-    if (i>=times) {
-        if (j < times || times === 0) {
-            i = 1;
-        }
-        else {
-            clearInterval(interval);
-        }
-    }j++;
 }
 
 function playCootie(whichColor) {
-    var i = 0;
+    var i, len, li;
     //get letters in color name
-    for (; i < whichColor.length; i++) {
-        var li = '<li>' + whichColor[i] + '</li>';
+    for (i = 0, len = whichColor.length; i < len; i++) {
+        li = '<li>' + whichColor[i] + '</li>';
         $('#progress').append(li);
+        //$('#progress li').fadeIn(1000).delay(800).fadeOut(1000);
     }
     moveCootie(i);
+
 }
 
 function drawCootie() {
-    var i, $canvas, ctx, w, h, strokeColor, fillColors, rand, squareColor;
-    i = 0;
-    w = 140;
-    h = 140;
-    strokeColor = '#999';
-    fillColors = ['red', 'green', 'blue', 'yellow'];
+    var $canvas, ctx, rand, squareColor;
+    var i = 0,
+        w = 140,
+        h = 140,
+        strokeColor = '#999',
+        fillColors = ['red', 'green', 'blue', 'yellow'];
+
     for (; i < 4; i++) {
         $canvas = $(document.createElement('canvas')).prop({
             'width': w,
             'height': h
         }).appendTo('.cootie');
     }
-    if ($('.cootie canvas', '#main').length) {
-        $('.cootie canvas', '#main').each(function() {
+    if ($('.cootie canvas').length) {
+        $('.cootie canvas').each(function() {
             rand = Math.floor(Math.random() * fillColors.length);
             squareColor = fillColors.splice(rand, 1)[0];
             ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
             ctx.strokeStyle = strokeColor;
             ctx.fillStyle = squareColor;
             ctx.fillRect(0, 0, w, h);
-            $(this).addClass(squareColor);
+            $(this).addClass('target ' + squareColor);
             $(this).click(function(e) {
                 var clr = e.target.className.split(' ').pop();
                 //get last class name, since that has the color
-                playCootie(clr);
+                if ($(this).is('.target')) {
+                    playCootie(clr);
+                    $('.cootie canvas').removeClass('target');
+                }
+
             });
+
         });
     }
 
