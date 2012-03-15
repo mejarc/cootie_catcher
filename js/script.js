007a6bcdc5d3650fc1ad8195c276d43e94ad7bb3
Melanie Archer <job@twobanjos.com>
2012-03-15 14:28:19 -0700

    These seem missing.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 //http://www.banane.com/2010/02/07/how-cootie-catcher-is-made/
 
 function moveCootie(times) { //blog: looping this is a nightmare/lesson in setInterval;
-    var rt, lt, $cootie = $('.cootie.opened'),
-        start = times - 1 || 1,
-        skew = function() {
-            lt = setInterval(function() {
-                $cootie.toggleClass('skew');
-            }, 800);
+    var i = 0,
+        $cootie = $('.cootie.container');
+    console.log('times:' + times);
+    console.log($cootie.hasClass('skew'));
+    for (; i <= times; i++) {//how to get this to apply/reapply xnumber of times??
+        if ($cootie.is('skew')) {
+            $cootie.removeClass('skew');
+        }
+        else {
+            $cootie.addClass('skew');
+        }
+    }
 
-            rt = setInterval(function() {
-                $cootie.toggleClass('revskew');
-            }, 800);
-            start++;
-            if (start > times) { alert('here');
-                clearInterval(lt);
-                clearInterval(rt);
-            }
-        };
-    skew();
 
+    //   lt = setInterval(function() {
+    //        $cootie.toggleClass('skew');
+    //   }, 800);
+    //  rt = setInterval(function() {
+    //      $cootie.toggleClass('revskew');
+    //  }, 800);
+    //  start++;
 }
 
 function playCootie(whichColor) {
@@ -33,41 +36,32 @@ function playCootie(whichColor) {
 }
 
 function drawCootie() {
-    var $canvas, ctx, rand, squareColor, i = 0,
-        w = 140,
-        h = 140,
-        strokeColor = '#999',
+    var rand, i = 0,
         fillColors = ['red', 'green', 'blue', 'yellow'];
 
+    console.log(i);
     for (; i < 4; i++) {
-        $canvas = $(document.createElement('canvas')).prop({
-            'width': w,
-            'height': h
-        }).appendTo('.cootie');
+        $('<div class="cootie square">').prependTo($('.cootie.container'));
     }
-    if ($('.cootie canvas').length) {
-        $('.cootie canvas').each(function() {
+    if ($('.cootie.container .square').length) {
+        $('.cootie.container .square').each(function() {
             rand = Math.floor(Math.random() * fillColors.length);
             squareColor = fillColors.splice(rand, 1)[0];
-            ctx = $(this)[0].getContext('2d'); //weird. 'Canvas' returns an array?! Blog
-            ctx.strokeStyle = strokeColor;
-            ctx.fillStyle = squareColor;
-            ctx.fillRect(0, 0, w, h);
+            console.log(squareColor);
+
             $(this).addClass('target ' + squareColor);
             $(this).click(function(e) {
                 var clr = e.target.className.split(' ').pop();
                 //get last class name, since that has the color
                 if ($(this).is('.target')) {
                     playCootie(clr);
-                    $('.cootie canvas').removeClass('target');
+                    $('.target').removeClass('target');
                 }
 
             });
 
         });
     }
-
-
 }
 $(function() {
     drawCootie();
